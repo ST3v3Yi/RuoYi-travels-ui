21eb40730336f1389f6390c8d3cb2677c9bdc5ad
ST3v3Yi <1157975832@qq.com>
2023-04-01 19:19:11 +0800

    init

--- src/views/route/writeRoute.vue
+++ src/views/route/writeRoute.vue
@@ -16,16 +16,22 @@
       class="backgroundImg"
       :src="imageUrl"
       style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
-      :preview-src-list="[imageUrl]">
+      :preview-src-list="[imageUrl]"
+      ref="image"
+      v-show="uploadedImage">
     </el-image>
   </div>
 </template>
 
 <script>
+import Cropper from 'cropperjs';
+import 'cropperjs/dist/cropper.css';
 export default {
   data() {
     return{
-      imageUrl: '',
+      imageUrl: require('@/assets/images/coverImg-background.jpg'),
+      uploadedImage: false,
+      cropper: null
     }
   },
   mounted() {
@@ -39,8 +45,60 @@ export default {
       const element = document.querySelector('.coverImg');
       element.style.width = `${width}px`;
       element.style.height = `${height}px`;
+    },
+    beforeUpload(file) {
+      const isLt5M = file.size / 1024 / 1024 < 5;
+      if (!isLt5M) {
+        this.$message.error('上传封面图片大小不能超过 5MB!');
+      }
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.imageUrl = e.target.result;
+        this.uploadedImage = true;
+        this.initCropper();
+      };
+      reader.readAsDataURL(file);
+      return false; // 阻止自动上传
+    },
+    handleUploadSuccess() {
+      // 上传成功后的回调
+    },
+    initCropper() {
+      if (this.cropper) {
+        this.cropper.destroy();
+      }
+      this.$nextTick(() => {
+        this.cropper = new Cropper(this.$refs.image.$el, {
+          aspectRatio: 1920 / 640,
+          viewMode: 1,
+          zoomable: false,
+          background: false,
+          cropBoxResizable: false,
+          autoCropArea: 1,
+          ready: () => {
+            this.cropper.setCropBoxData({
+              width: 1920,
+              height: 640
+            });
+          }
+        });
+      });
+    },
+    cropImage() {
+      if (this.cropper) {
+        const croppedCanvas = this.cropper.getCroppedCanvas({
+          width: 1920,
+          height: 640
+        });
+        this.imageUrl = croppedCanvas.toDataURL();
+      }
     }
   },
+  beforeDestroy() {
+    if (this.cropper) {
+      this.cropper.destroy();
+    }
+  }
 }
 </script>
 
