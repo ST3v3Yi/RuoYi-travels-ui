9907a7a2f9ccc7dbde74afb72540ed8f8079729a
ST3v3Yi <1157975832@qq.com>
2023-04-02 16:53:22 +0800

    init

--- src/views/route/writeRoute.vue
+++ src/views/route/writeRoute.vue
@@ -1,22 +1,26 @@
 <template>
-  <div class="coverImg">
-    <div class="uploadContainer">
-      <el-upload
-        class="uploadCoverImg"
-        drag
-        action="">
-        <i class="el-icon-upload"></i>
-        <div class="el-upload__text">将文件拖到此处，或<em>点击上传封面图片</em></div>
-        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过5MB</div>
-      </el-upload>
+  <div>
+    <div class="coverImg">
+      <div class="uploadContainer">
+        <el-upload
+          class="uploadCoverImg"
+          drag
+          action=""
+          @change="handleUploadSuccess">
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">将文件拖到此处，或<em>点击上传封面图片</em></div>
+          <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过5MB</div>
+        </el-upload>
+      </div>
+      <el-image
+        class="backgroundImg"
+        :src="imageUrl"
+        >
+      </el-image>
     </div>
-    <el-image
-      class="backgroundImg"
-      :src="imageUrl"
-      :preview-src-list="[imageUrl]"
-      ref="image"
-      >
-    </el-image>
+    <div class="titleContainer">
+      <el-input class="titleInput" v-model="title" placeholder="请输入游记名称"></el-input>
+    </div>
   </div>
 </template>
 
@@ -26,13 +30,17 @@ import 'cropperjs/dist/cropper.css';
 export default {
   data() {
     return {
-      imageUrl: require('@/assets/images/coverImg-background.jpg')
+      imageUrl: require('@/assets/images/coverImg-background.jpg'),
+      title: '',
     }
   },
   mounted() {
+
   },
   methods: {
+    handleUploadSuccess() {
 
+    }
   }
 }
 </script>
@@ -40,6 +48,8 @@ export default {
 <style>
 .coverImg {
   position: relative;
+  width: 100%;
+  height: 640px;
 }
 .uploadContainer {
   display: flex;
@@ -62,5 +72,22 @@ export default {
   width: 100%;
   height: 100%;
   z-index: 0;
+}
+.titleContainer {
+  display: flex;
+  justify-content: center;
+  margin-top: -70px;
+}
+.titleInput {
+  width: 980px;
+}
+.titleInput:focus {
+  outline: 10px solid #1ab394;
+}
+.el-input--medium .el-input__inner {
+  height: 62px;
+  font-size: 18px;
+  border-radius: 0px;
+  padding: 10px;
 }
 </style>
