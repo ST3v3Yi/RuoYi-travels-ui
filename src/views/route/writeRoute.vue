f05d5b5c00891e16e5bd8e591274455144390ba2
ST3v3Yi <1157975832@qq.com>
2023-04-02 21:14:56 +0800

    init

--- src/views/route/writeRoute.vue
+++ src/views/route/writeRoute.vue
@@ -2,7 +2,27 @@
   <div>
     <div class="coverImg">
       <div class="uploadContainer">
-        <imageUpload class="uploadCoverImg" v-model="uploadImg" @upload-success="handleUploadSuccess" v-show="showUpload" />
+        <el-upload
+          class="uploadCoverImg"
+          v-model="uploadImg"
+          v-show="showUpload"
+          drag
+          :action="uploadImgUrl"
+          :on-success="handleUploadSuccess"
+          :before-upload="handleBeforeUpload"
+          :limit="limit"
+          :on-error="handleUploadError"
+          :on-exceed="handleExceed"
+          :on-remove="handleDelete"
+          :show-file-list="true"
+          :headers="headers"
+          :file-list="fileList"
+          :class="{hide: this.fileList.length >= this.limit}"
+        >
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+          <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过5MB</div>
+        </el-upload>
       </div>
       <el-image
         class="backgroundImg"
@@ -10,38 +30,194 @@
         >
       </el-image>
     </div>
-    <div class="titleContainer">
-      <el-input class="titleInput" v-model="title" placeholder="请输入游记名称"></el-input>
+    <div class="inputContainer">
+      <div>
+        <el-button type="danger" icon="el-icon-delete" circle class="imgDel" @click="coverDel" v-show="showDelButton"></el-button>
+      </div>
+      <div class="titleContainer">
+        <el-input class="titleInput" v-model="title" placeholder="请输入游记名称"></el-input>
+      </div>
     </div>
-    <img :src="coverUrl" />
   </div>
 </template>
 
 <script>
 import Cropper from 'cropperjs';
 import 'cropperjs/dist/cropper.css';
-import imageUpload from '@/components/ImageUpload/upload.vue'
+import { getToken } from "@/utils/auth";
 export default {
+  props: {
+    value: [String, Object, Array],
+    // 图片数量限制
+    limit: {
+      type: Number,
+      default: 1,
+    },
+    // 大小限制(MB)
+    fileSize: {
+      type: Number,
+      default: 5,
+    },
+    // 文件类型, 例如['png', 'jpg', 'jpeg']
+    fileType: {
+      type: Array,
+      default: () => ["png", "jpg", "jpeg"],
+    },
+    // 是否显示提示
+    isShowTip: {
+      type: Boolean,
+      default: true
+    }
+  },
   data() {
     return {
+      number: 0,
+      uploadList: [],
+      dialogImageUrl: "",
+      dialogVisible: false,
+      hideUpload: false,
+      baseUrl: process.env.VUE_APP_BASE_API,
+      uploadImgUrl: process.env.VUE_APP_BASE_API + "/common/upload", // 上传的图片服务器地址
+      headers: {
+        Authorization: "Bearer " + getToken(),
+      },
+      fileList: [],
       imageUrl: require('@/assets/images/coverImg-background.jpg'),
       title: '',
       uploadImg: '',
-      coverUrl: '',
       showUpload: true,
+      showDelButton: false,
+    };
+  },
+  watch: {
+    value: {
+      handler(val) {
+        if (val) {
+          // 首先将值转为数组
+          const list = Array.isArray(val) ? val : this.value.split(',');
+          // 然后将数组转为对象数组
+          this.fileList = list.map(item => {
+            if (typeof item === "string") {
+              if (item.indexOf(this.baseUrl) === -1) {
+                item = { name: this.baseUrl + item, url: this.baseUrl + item };
+              } else {
+                item = { name: item, url: item };
+              }
+            }
+            return item;
+          });
+        } else {
+          this.fileList = [];
+          return [];
+        }
+      },
+      deep: true,
+      immediate: true
     }
   },
-  components: {
-    imageUpload,
-  },
-  mounted() {
-
+  computed: {
+    // 是否显示提示
+    showTip() {
+      return this.isShowTip && (this.fileType || this.fileSize);
+    },
   },
   methods: {
-    handleUploadSuccess(url) {
-      this.imageUrl = '/dev-api' + url;
-      this.showUpload = false;
-      console.log(this.imageUrl);
+    // 上传前loading加载
+    handleBeforeUpload(file) {
+      let isImg = false;
+      if (this.fileType.length) {
+        let fileExtension = "";
+        if (file.name.lastIndexOf(".") > -1) {
+          fileExtension = file.name.slice(file.name.lastIndexOf(".") + 1);
+        }
+        isImg = this.fileType.some(type => {
+          if (file.type.indexOf(type) > -1) return true;
+          if (fileExtension && fileExtension.indexOf(type) > -1) return true;
+          return false;
+        });
+      } else {
+        isImg = file.type.indexOf("image") > -1;
+      }
+
+      if (!isImg) {
+        this.$modal.msgError(`文件格式不正确, 请上传${this.fileType.join("/")}图片格式文件!`);
+        return false;
+      }
+      if (this.fileSize) {
+        const isLt = file.size / 1024 / 1024 < this.fileSize;
+        if (!isLt) {
+          this.$modal.msgError(`上传头像图片大小不能超过 ${this.fileSize} MB!`);
+          return false;
+        }
+      }
+      this.$modal.loading("正在上传图片，请稍候...");
+      this.number++;
+    },
+    // 文件个数超出
+    handleExceed() {
+      this.$modal.msgError(`上传文件数量不能超过 ${this.limit} 个!`);
+    },
+    // 上传成功回调
+    handleUploadSuccess(res, file) {
+      if (res.code === 200) {
+        this.uploadList.push({ name: res.fileName, url: res.fileName });
+        this.uploadedSuccessfully();
+      } else {
+        this.number--;
+        this.$modal.closeLoading();
+        this.$modal.msgError(res.msg);
+        this.$refs.imageUpload.handleRemove(file);
+        this.uploadedSuccessfully();
+      }
+    },
+    // 删除图片
+    handleDelete(file) {
+      const findex = this.fileList.map(f => f.name).indexOf(file.name);
+      if(findex > -1) {
+        this.fileList.splice(findex, 1);
+        this.$emit("input", this.listToString(this.fileList));
+      }
+    },
+    // 上传失败
+    handleUploadError() {
+      this.$modal.msgError("上传图片失败，请重试");
+      this.$modal.closeLoading();
+    },
+    // 上传结束处理
+    uploadedSuccessfully() {
+      if (this.number > 0 && this.uploadList.length === this.number) {
+        this.fileList = this.fileList.concat(this.uploadList);
+        this.uploadList = [];
+        this.number = 0;
+        this.$emit("input", this.listToString(this.fileList));
+        this.$modal.closeLoading();
+        this.imageUrl = '/dev-api' + this.fileList[0].url;
+        this.showUpload = false;
+        this.showDelButton = true;
+        console.log(this.fileList[0].url);
+      }
+    },
+    /*// 预览
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    },*/
+    // 对象转成指定字符串分隔
+    listToString(list, separator) {
+      let strs = "";
+      separator = separator || ",";
+      for (let i in list) {
+        if (list[i].url) {
+          strs += list[i].url.replace(this.baseUrl, "") + separator;
+        }
+      }
+      return strs != '' ? strs.substr(0, strs.length - 1) : '';
+    },
+    coverDel() {
+      this.handleDelete(this.fileList[0]);
+      this.imageUrl = require('@/assets/images/coverImg-background.jpg');
+      this.showUpload = true;
+      this.showDelButton = false;
     }
   }
 }
@@ -75,10 +251,19 @@ export default {
   height: 100%;
   z-index: 0;
 }
-.titleContainer {
+.inputContainer {
   display: flex;
-  justify-content: center;
+  flex-direction: column;;
+  align-items: center;
+  position: relative;
+}
+.titleContainer {
   margin-top: -70px;
+}
+.imgDel {
+  position: absolute;
+  top: -110px;
+  left: 500px;
 }
 .titleInput {
   width: 980px;
