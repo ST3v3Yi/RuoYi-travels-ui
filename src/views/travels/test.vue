df79bd16b3cbaf573ec6c224e9aac5f4353b0bc8
ST3v3Yi <1157975832@qq.com>
2023-05-02 22:47:48 +0800

    init

--- src/views/travels/test.vue
+++ src/views/travels/test.vue
@@ -37,23 +37,118 @@
           </div>
           <el-button type="text" style="position: absolute; bottom: 5px; right: 20px;" @click="confirmRoom">确定</el-button>
         </div>
+        <div class="hotelFilter">
+          <!-- 价格筛选 -->
+          <div style="display: flex; width: 100%; height: 90px; border-bottom: 1px solid #eee">
+            <div class="aside">
+              <span style="font-size: 16px; font-weight: bold; color: #333">酒店价格</span>
+            </div>
+            <div class="roomPrice">
+              <div class="priceButton">
+                <el-button :type="roomPrice===0 ? 'primary' : 'text'" @click="selectPrice(0, 150)" style="margin-left: 5px;">150以下</el-button>
+                <el-button :type="roomPrice===150 ? 'primary' : 'text'" @click="selectPrice(150, 300)">150-300</el-button>
+                <el-button :type="roomPrice===300 ? 'primary' : 'text'" @click="selectPrice(300, 450)">300-450</el-button>
+                <el-button :type="roomPrice===450 ? 'primary' : 'text'" @click="selectPrice(450, 600)">450-600</el-button>
+                <el-button :type="roomPrice===600 ? 'primary' : 'text'" @click="selectPrice(600, 0)">600以上</el-button>
+              </div>
+              <div class="priceFilter">
+                <div @mouseover="showButtons = true" @mouseleave="showButtons = false" style="margin-left: 5px; position: relative;">
+                  <span>￥</span>
+                  <el-input v-model="price.minPrice" style="width: 80px;" placeholder="最低价"></el-input>
+                  <span style="white-space: pre;"> -￥</span>
+                  <el-input v-model="price.maxPrice" style="width: 80px;" placeholder="最高价"></el-input>
+                  <div v-if="showButtons" class="showButton">
+                    <el-button
+                      type="primary"
+                      style="background-color: #666666; margin-left: 65px"
+                      @click="clearInput"
+                    >清空</el-button>
+                    <el-button
+                      type="primary"
+                      style="margin-left: 5px"
+                      @click="selectByPrice"
+                    >确定</el-button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- 酒店评分 -->
+          <div style="display: flex; width: 100%; height: 50px; border-bottom: 1px solid #eee">
+            <div class="aside">
+              <span style="font-size: 16px; font-weight: bold; color: #333">酒店评分</span>
+            </div>
+            <div class="roomRating">
+              <el-button :type="roomRating===3 ? 'primary' : 'text'" @click="selectRating(3)" style="margin-left: 5px;">全部</el-button>
+              <el-button :type="roomRating===4.5 ? 'primary' : 'text'" @click="selectRating(4.5)">4.5分以上</el-button>
+              <el-button :type="roomRating===4 ? 'primary' : 'text'" @click="selectRating(4)">4分以上</el-button>
+              <el-button :type="roomRating===3.5 ? 'primary' : 'text'" @click="selectRating(3.5)">3.5分以上</el-button>
+            </div>
+          </div>
+          <!-- 酒店房型 -->
+          <div style="display: flex; width: 100%; height: 50px; border-bottom: 1px solid #eee">
+            <div class="aside">
+              <span style="font-size: 16px; font-weight: bold; color: #333">酒店房型</span>
+            </div>
+            <div style="display: flex; align-items: center;">
+              <el-button :type="roomLayout===3 ? 'primary' : 'text'" @click="selectLayout(3)" style="margin-left: 5px;">全部</el-button>
+              <el-button :type="roomLayout===0 ? 'primary' : 'text'" @click="selectLayout(0)">双床房</el-button>
+              <el-button :type="roomLayout===1 ? 'primary' : 'text'" @click="selectLayout(1)">大床房</el-button>
+              <el-button :type="roomLayout===2 ? 'primary' : 'text'" @click="selectLayout(2)">多床房</el-button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="mapContainer">
+    <div class="infoContainer">
+      <div class="hotelContainer">
+<!--        <div class="hotelInfoFilter"></div>-->
+        <div class="hotelCard">
+          <el-card
+            v-for="(item, index) in hotelList"
+            :key="index"
+            style="margin-bottom: 5px;">
+            <div class="hotelInfo">
+              <div>
+                <img :src="'dev-api' + item.coverImg" style="width: 200px; height: 160px;"/>
+              </div>
+              <router-link :to="{ path: '/hotelDetail/:id', query: { id: item.id } }">
+              <div class="hotelName" @mouseover="hoverHotelCard(item)" @mouseout="leaveHotelCard">
+                <h1>{{item.hotelName}}</h1>
+                <h2>{{ item.hotelForeignName }}</h2>
+              </div>
+              </router-link>
+              <div class="hotelReviews">
+                <div class="hotelRating" v-if="item.rating && item.rating.mainRating">
+                  <span style="font-size: 14px; font-weight: bold; color: #FFFFFF">{{ item.rating.mainRating | decimal }}</span>
+                </div>
+              </div>
+              <div class="price">
+                <p>￥{{ item.minPrice }}<strong style="font-size: 14px; font-weight: normal; color: #999999;">起</strong></p>
+                <router-link :to="{ path: '/hotelDetail/:id', query: { id: item.id } }">
+                  <el-button type="primary">查看详情</el-button>
+                </router-link>
+              </div>
+            </div>
+          </el-card>
+        </div>
+      </div>
       <div class="mapWrapper">
         <baidu-map
           class="map"
           :center="center"
-          :zoom="14"
+          :zoom="mapZoom"
           :scroll-wheel-zoom="false">
           <bm-marker
             v-for="item in hotelList"
             :position="item.point"
-            :icon="{ url: iconUrl, size: { width: 32, height: 32 } }"
-            @mouseover.stop="markerHover"
-            @mouseout.stop="markerHoverOut"
+            :icon="{
+              url: item.isHover ? item.hoverIconUrl : item.iconUrl,
+              size: { width: 32, height: 32 } }"
+            @mouseover="item.isHover = true"
+            @mouseout="item.isHover = false"
             @click="infoWindowOpen(item.id)">
-            <bm-info-window :show="isInfoWindow" @close="infoWindowClose" @open="infoWindowOpen">
+            <bm-info-window :show="isInfoWindow && item.id === openWinId" @close="infoWindowClose(item.id)" @open="infoWindowOpen(item.id)">
               <div class="locationInfo">
                 <h1>{{ item.hotelName }}</h1>
                 <div style="display: flex;">
@@ -82,14 +177,19 @@
 import { listHotel } from '@/api/hotel/hotel'
 import { getMinPrice } from "@/api/travels/rooms";
 import { getHotelRating } from "@/api/hotel/hotelComments";
+// 导入 Intl.NumberFormat
+import Intl from 'intl'
+import 'intl/locale-data/jsonp/en.js'
+import {getListByPrice} from "@/api/route/route";
 export default {
   data() {
     return {
       hotelList: [],
       date: new Date(),
       dateRange: [],
+      markers: {},
       isShowWin: false,
-      openWin: null,
+      openWinId: 0,
       buttonValue: '1间，1位',
       roomNum: 1,
       peopleNum: 1,
@@ -97,8 +197,16 @@ export default {
         lng: 114.347061,
         lat: 34.815321
       },
-      iconUrl: require('@/assets/location.png'),
+      mapZoom: 12,
       isInfoWindow: false,
+      roomPrice: null,
+      showButtons: false,
+      price: {
+        minPrice: null,
+        maxPrice: null
+      },
+      roomRating: 3,
+      roomLayout: 3,
     }
   },
   mounted() {
@@ -128,6 +236,10 @@ export default {
         this.hotelList.forEach((hotel) => {
           getMinPrice(hotel.id).then((res) => {
             hotel.minPrice = res.data;
+            hotel.minPrice = new Intl.NumberFormat('zh-CN', {
+              style: 'currency',
+              currency: 'CNY',
+            }).format(hotel.minPrice).replace(/^(\D+)/, '').replace(/\.\d{2}$/, '');
           })
           getHotelRating(hotel.id).then((res) => {
             hotel.rating = res.data;
@@ -138,8 +250,11 @@ export default {
             lat: hotel.lat
           };
           hotel.point = markerCenter;
+          hotel.iconUrl = require('@/assets/location.png');
+          hotel.hoverIconUrl = require('@/assets/location1.png');
+          hotel.isHover = false;
         })
-        console.log(this.hotelList);
+        console.log(this.hotelList)
       })
       const today = this.today;
       const tomorrow = this.tomorrow;
@@ -154,22 +269,63 @@ export default {
       this.buttonValue = this.roomNum + "间，" + this.peopleNum + "位";
       this.isShowWin = false;
     },
-    markerHover() {
-      this.iconUrl = require('@/assets/location1.png');
-    },
-    markerHoverOut() {
-      this.iconUrl = require('@/assets/location.png');
-    },
     // 位置Marker点击事件
     infoWindowOpen(id) {
+      this.openWinId = id;
       this.isInfoWindow = true;
-      this.openWin = id;
     },
     // 位置Marker关闭事件
     infoWindowClose() {
       this.isInfoWindow = false;
-      this.openWin = null;
-    }
+      this.openWinId = 0;
+    },
+    // 选择酒店价格
+    selectPrice(min, max) {
+      if (this.roomPrice !== min) {
+        this.roomPrice = min;
+      } else {
+        this.roomPrice = null;
+      }
+    },
+    clearInput() {
+      this.price.minPrice = '';
+      this.price.maxPrice = '';
+    },
+    selectByPrice() {
+      console.log(this.minPrice, this.maxPrice)
+      if (this.minPrice > this.maxPrice) {
+        this.$notify.error({
+          title: '错误',
+          message: '最低值不得高于最高值！'
+        })
+      } else {
+        // this.loading = true;
+        // const data = this.price;
+        // getListByPrice(data).then((res) => {
+        //   this.routeList = res.data;
+        //   this.loading = false;
+        // })
+      }
+    },
+    // 选择酒店评分
+    selectRating(id) {
+      this.roomRating = id;
+    },
+    // 选择房型
+    selectLayout(id) {
+      this.roomLayout = id;
+    },
+    hoverHotelCard(item) {
+      this.center = item.point;
+      this.infoWindowOpen(item.id);
+    },
+    leaveHotelCard() {
+      this.center = {
+        lng: 114.347061,
+        lat: 34.815321
+      };
+      this.infoWindowClose();
+    },
   },
   filters: {
     decimal(value) {
@@ -190,9 +346,8 @@ export default {
   position: relative;
   width: 1050px;
   margin-top: 5px;
-  padding: 10px;
+  margin-bottom: 10px;
   border-radius: 5px;
-  background-color: #666666;
   .chooseWindow {
     position: absolute;
     width: 250px;
@@ -225,23 +380,169 @@ export default {
       border: 0px;
     }
   }
+  .hotelFilter {
+    width: 100%;
+    margin-top: 10px;
+    border-radius: 5px;
+    box-shadow: 0 2px 12px 0 #eee;
+    border: 1px solid #eee;
+    .aside {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 150px;
+      background-color: #f5f7fa;
+    }
+    .roomPrice {
+      display: flex;
+      align-items: center;
+      flex-wrap: wrap;
+      height: auto;
+      .priceButton {
+        display: flex;
+        align-items: center;
+        flex-basis: 100%;
+        margin-top: 5px;
+      }
+      .priceFilter {
+        display: flex;
+        align-items: center;
+        margin-top: 5px;
+        margin-bottom: 20px;
+        ::v-deep .el-input--medium .el-input__inner {
+          height: 32px;
+          line-height: 32px;
+        }
+        .showButton {
+          display: flex;
+          margin-left: 33px;
+          margin-top: 5px;
+          .el-button {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 50px;
+            height: 30px;
+            font-size: 14px;
+          }
+          ::v-deep .el-button--medium {
+            padding: 10px 0px;
+          }
+        }
+      }
+    }
+    .roomRating {
+      display: flex;
+      align-items: center;
+      .el-button {
+        width: 70px;
+        font-size: 14px;
+      }
+      ::v-deep .el-button--medium {
+        padding: 10px 0px;
+      }
+    }
+    .el-button {
+      width: 60px;
+      font-size: 14px;
+    }
+    ::v-deep .el-button--medium {
+      padding: 10px 0px;
+    }
+  }
 }
-.mapContainer {
+.infoContainer {
   display: flex;
   justify-content: center;
   width: 100%;
+  .hotelContainer {
+    width: 640px;
+    margin-right: 10px;
+    .hotelInfoFilter {
+      width: 100%;
+      height: 40px;
+      margin-bottom: 10px;
+      border-radius: 5px;
+      background-color: #eeeeee;
+    }
+    .hotelCard {
+      width: 100%;
+      .hotelInfo {
+        position: relative;
+        display: flex;
+        .hotelName {
+          h1 {
+            font-size: 20px;
+            font-weight: bold;
+            cursor: pointer;
+            margin: 0;
+            padding: 5px 5px 0 10px;
+          }
+          h1:hover {
+            color: #1ab394;
+          }
+          h2 {
+            font-size: 14px;
+            margin: 0 0 0 10px;
+            color: #999999;
+          }
+        }
+        .hotelReviews {
+          position: absolute;
+          top: 0;
+          right: 0;
+          .hotelRating {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 40px;
+            height: 25px;
+            border-radius: 5px;
+            background-color: #1ab394;
+          }
+        }
+        .price {
+          position: absolute;
+          right: 0;
+          bottom: 0;
+          text-align: right;
+          p {
+            font-size: 24px;
+            font-weight: bold;
+            color: #1ab394;
+            margin: 0;
+          }
+          ::v-deep .el-button--medium {
+            font-size: 16px;
+          }
+          ::v-deep .el-button--primary {
+            background-color: #1ab394;
+          }
+        }
+      }
+      ::v-deep .el-card.is-always-shadow {
+        box-shadow: 0 0 0 0 #FFFFFF;
+      }
+      ::v-deep .el-card:hover {
+        box-shadow: 0 2px 5px 0 #999;
+      }
+      ::v-deep .el-card__body {
+        padding: 15px 20px 15px 20px;
+      }
+    }
+  }
   .mapWrapper {
     display: flex;
     justify-content: center;
-    width: 1050px;
-    height: 420px;
+    width: 400px;
+    height: 400px;
     border: 1px solid #eee;
     border-radius: 5px;
-    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+    box-shadow: 0 2px 12px 0 #eeeeee;
   }
   .map {
-    width: 1050px;
-    height: 420px;
+    width: 100%;
+    height: 100%;
   }
   .locationInfo {
     position: relative;
